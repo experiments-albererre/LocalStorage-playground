465608c754939326b5bdaed6277125709d201c8b
experiments-albererre <38617665+experiments-albererre@users.noreply.github.com>
2019-01-29 17:04:50 +0100

    Create localstore.js

---
+++ localstore.js
@@ -0,0 +1,9 @@
+// LocalStore
+localStorage.setItem('pepe', JSON.stringify({name: "Pepe", origin: "Madrid"}));
+
+console.log(JSON.parse(localStorage.getItem('pepe')));
+
+// session storage
+sessionStorage.setItem('pepe', JSON.stringify({name: "PepeSession", origin: "PepeSession"}));
+
+console.log(JSON.parse(sessionStorage.getItem('pepe')));
